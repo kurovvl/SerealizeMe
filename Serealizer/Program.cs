4e0bc2f02ac5d7a276b518c126d7312e237415b4
kurovvl <kurovvl@>
2024-03-18 15:50:58 +0300

    some fixes

--- Serealizer/Program.cs
+++ Serealizer/Program.cs
@@ -2,22 +2,22 @@
 using Serealizer.Test;
 
 
-WatchTest.WatchAction mySerAction = () => Serealizer.Classes.Serealizer.Serealize(TestClass.Get());
+WatchTest.WatchAction MySerAction = () => Serealizer.Classes.Serealizer.Serealize(TestClass.Get());
 WatchTest.WatchAction NewtonSerAction = () => Newtonsoft.Json.JsonConvert.SerializeObject(TestClass.Get());
 
-string myJsonStr = (string)mySerAction();
+string MyJsonStr = (string)MySerAction();
 string NewtonJsonStr = (string)NewtonSerAction();
 
-WatchTest.WatchAction MyDeserAction = () => Serealizer.Classes.Serealizer.Deserealise<TestClass>(myJsonStr);
+WatchTest.WatchAction MyDeserAction = () => Serealizer.Classes.Serealizer.Deserealise<TestClass>(MyJsonStr);
 WatchTest.WatchAction NewtonDeserAction = () => Newtonsoft.Json.JsonConvert.DeserializeObject<TestClass>(NewtonJsonStr);
 
 
-TestClass myDeserObj = (TestClass)MyDeserAction();
+TestClass MyDeserObj = (TestClass)MyDeserAction();
 TestClass NewtonDeserObj = (TestClass)NewtonDeserAction();
 
-Console.WriteLine($"Сереализовано: {myJsonStr}");
-Console.WriteLine($"Сереализовано by Newton: {myJsonStr}");
-Console.WriteLine($"Десереализация одинакова: {myDeserObj.Equals(NewtonDeserObj)}");
+Console.WriteLine($"Сереализовано: {MyJsonStr}");
+Console.WriteLine($"Сереализовано by Newton: {MyJsonStr}");
+Console.WriteLine($"Десереализация одинакова: {MyDeserObj.Equals(NewtonDeserObj)}");
 
 Console.WriteLine("\n");
 
@@ -29,9 +29,9 @@ WatchTest watch = new WatchTest(10000);
 for (int i = 0; i < 10; i++)
 {
     Console.WriteLine($"Тест #{i + 1}");
-    Console.WriteLine($"Моя сериализация: {watch.Watch(mySerAction)}");
-    Console.WriteLine($"Моя десериализация: {watch.Watch(NewtonSerAction)}");
-    Console.WriteLine($"Newton сериализация: {watch.Watch(MyDeserAction)}");
+    Console.WriteLine($"Моя сериализация: {watch.Watch(MySerAction)}");
+    Console.WriteLine($"Моя десериализация: {watch.Watch(MyDeserAction)}");
+    Console.WriteLine($"Newton сериализация: {watch.Watch(NewtonSerAction)}");
     Console.WriteLine($"Newton десериализация: {watch.Watch(NewtonDeserAction)}");
     Console.WriteLine($"---------");
 
